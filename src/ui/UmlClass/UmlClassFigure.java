4c0bfbd9d96ae2b44787d390dbab68bdc06fa276
hendleyr <hendleyr@gmail.com>
2012-10-14 01:17:50 -0500

    UmlClassFigure is actually *doing* something interesting now! (as a demo, try inputting whatever you'd like for class names on class figures. It should reset to something valid no matter what *and* find the access modifier character (as long as its at index 0...)) I've added listeners that will check for any change to the figure and remap the data into model objects as needed. I'm using some regular expressions and a lot of string examination to make sure we're always pumping valid data into the models and maintaining a consistent state between the ui and model.
    However, I've run into a snag with the AttributesCompartment not updating
    as I'd expect; the listener doesn't seem to be getting fresh data, and
    so it's not actually checking edits made by users. The class name text
    figure is working though, so I'm not sure why there's this weirdness here.
    
    Signed-off-by: hendleyr <hendleyr@gmail.com>

--- src/ui/UmlClass/UmlClassFigure.java
+++ src/ui/UmlClass/UmlClassFigure.java
@@ -14,10 +14,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.Action;
 
 import org.jhotdraw.app.action.edit.AbstractSelectionAction;
+import org.jhotdraw.draw.AttributeKey;
+import org.jhotdraw.draw.AttributeKeys;
 import org.jhotdraw.draw.Figure;
 import org.jhotdraw.draw.GraphicalCompositeFigure;
 import org.jhotdraw.draw.LineConnectionFigure;
@@ -32,23 +38,57 @@ import org.jhotdraw.geom.Insets2D;
 import org.jhotdraw.samples.pert.figures.SeparatorLineFigure;
 import org.jhotdraw.util.ResourceBundleUtil;
 
+import sun.misc.Regexp;
+
+import domain.UmlClass.AccessModifier;
+import domain.UmlClass.UmlAttributeModel;
 import domain.UmlClass.UmlClassModel;
+import domain.UmlClass.UmlMethodModel;
 
 
 @SuppressWarnings("serial")
 public class UmlClassFigure extends GraphicalCompositeFigure {
+	private int uniqAttrId = 1;
+	private int uniqMethodId = 1;
     private static class ClassNameAdapter extends FigureAdapter {
         private UmlClassFigure target;
 
         public ClassNameAdapter(UmlClassFigure target) {
             this.target = target;
         }
-
+        
         @Override
-        public void attributeChanged(FigureEvent e) {
-            // We could fire a property change event here, in case
-            // some other object would like to observe us.
-            //target.firePropertyChange("name", e.getOldValue(), e.getNewValue());
+        public void figureChanged(FigureEvent e) {
+        	// VERIFY AND MAP USER INPUT TO MODEL
+        	TextFigure nameFigure = target.getNameFigure();        	        	
+        	String text = nameFigure.getText();
+        	
+        	AccessModifier modelAccessModifier;
+        	if (text.startsWith("+")) {
+        		modelAccessModifier = AccessModifier.Public;
+        	}
+        	else if (text.startsWith("-")) {
+        		modelAccessModifier = AccessModifier.Private;
+        	}
+        	else if (text.startsWith("#")) {
+        		modelAccessModifier = AccessModifier.Protected;
+        	}
+        	else modelAccessModifier = AccessModifier.Local;
+        	
+        	Pattern p = Pattern.compile("([\\w]+\\.)*[\\w]+");
+        	Matcher m = p.matcher(text);
+        	m.find();
+        	target.getModel().setAccessModifier(modelAccessModifier);
+        	if (m.group().length() > 0) {
+        		// found at least one valid identifier
+        		nameFigure.setText(modelAccessModifier.getSymbol() + " " + m.group());
+        		target.getModel().setName(m.group());
+        	}
+        	else {
+        		// regex failed to find a valid java identifer
+        		nameFigure.setText(modelAccessModifier.getSymbol() + " " + "newClass");
+        		target.getModel().setName("newClass");
+        	}
         }
     }
 
@@ -57,14 +97,70 @@ public class UmlClassFigure extends GraphicalCompositeFigure {
 
         public AttributeAdapter(UmlClassFigure target) {
             this.target = target;
-        }
+        }//TODO: split out these huge blocks
 
         @Override
-        public void attributeChanged(FigureEvent evt) {
-            // We could fire a property change event here, in case
-            // some other object would like to observe us.
-            //target.firePropertyChange("duration", e.getOldValue(), e.getNewValue());
-        }
+        public void figureChanged(FigureEvent e) {        	
+        	// VERIFY AND MAP USER INPUT TO MODELS
+        	// new strat: reinitialize the model's attr list; examine all text figures in  
+        	// the compartment, map them into attrModels, then re-add them to the classModel
+        	// if user entered bad data, we assign default values w/ uniqAttrId
+        	// TODO: bad design to use a try/catch on the re-add, refactor addAttribute to boolean...?
+        	List<Figure> attrFigures = target.getAttributesCompartment().getChildren();
+        	List<UmlAttributeModel> remappedAttrs = new ArrayList<UmlAttributeModel>();
+        	Pattern p = Pattern.compile("([\\w]+)");	// regex for words
+        	
+        	// reinit class model's attrList
+        	target.getModel().setAttributes(new ArrayList<UmlAttributeModel>());
+        	for(Figure attrFigure : attrFigures) {
+        		String attrText = ((TextFigure)attrFigure).getText();
+        		
+            	// assess <accessmod>
+            	AccessModifier attrAccessModifier;
+            	if (attrText.startsWith("+")) {
+            		attrAccessModifier = AccessModifier.Public;
+            	}
+            	else if (attrText.startsWith("-")) {
+            		attrAccessModifier = AccessModifier.Private;
+            	}
+            	else if (attrText.startsWith("#")) {
+            		attrAccessModifier = AccessModifier.Protected;
+            	}
+            	else attrAccessModifier = AccessModifier.Local;
+        		
+        		// assess <name> : <type>
+        		String attrType;
+        		String attrName;
+            	if (attrText.indexOf(':') == -1) {
+            		attrType = "Object";
+            		
+            		Matcher m = p.matcher(attrText.substring(0, attrText.indexOf(':')));
+            		m.find();
+            		attrName = m.group();
+            	}
+            	else {
+            		//TODO: use fully qualified java identifier pattern
+                	Matcher m = p.matcher(attrText.substring(attrText.indexOf(':')));
+                	m.find();
+                	attrType = m.group();
+            		
+                	m = p.matcher(attrText.substring(0, attrText.indexOf(':')));
+                	m.find();
+                	attrName = m.group();
+            	}
+            	
+            	UmlAttributeModel remappedAttr = new UmlAttributeModel(attrAccessModifier, attrName, attrType);
+            	try {
+            		target.getModel().addAttribute(remappedAttr);
+            	} catch (Exception mapException) {
+            		// something failed, supply defaults for user revision
+            		remappedAttr = new UmlAttributeModel
+            				(AccessModifier.Private, "newAttribute" + target.uniqAttrId, "Object");
+            	}
+            	((TextFigure)attrFigure).setText
+            		(attrAccessModifier.getSymbol() + remappedAttr.getName() + " : " + remappedAttr.getType());
+        	}
+    	}
     }
     
     private static class MethodAdapter extends FigureAdapter {
@@ -82,11 +178,7 @@ public class UmlClassFigure extends GraphicalCompositeFigure {
         }
     }
     
-    private HashSet<LineConnectionFigure> myAssociationFigures;
-	
-//    private Font attributeFont;
-//    private Font methodFont;
-    
+    private HashSet<LineConnectionFigure> myAssociationFigures;    
 	private UmlClassModel model;
 	
     public UmlClassModel getModel() {
@@ -98,8 +190,9 @@ public class UmlClassFigure extends GraphicalCompositeFigure {
     
     public UmlClassFigure() {
         super(new RectangleFigure());
+        setModel(new UmlClassModel());
+        
         setLayouter(new VerticalLayouter());
-
         RectangleFigure nameCompartmentPF = new RectangleFigure();
         nameCompartmentPF.set(STROKE_COLOR, null);
         nameCompartmentPF.set(FILL_COLOR, null);
@@ -127,11 +220,56 @@ public class UmlClassFigure extends GraphicalCompositeFigure {
         attributesCompartment.set(LAYOUT_INSETS, insets);
         methodsCompartment.set(LAYOUT_INSETS, insets);
 
-        nameCompartment.add(new TextFigure("newClass"));
-        attributesCompartment.add(new TextFigure("- newAttribute: Object"));
+        TextFigure nameFigure = new TextFigure("newClass");
+        nameCompartment.add(nameFigure);
+        nameFigure.addFigureListener(new ClassNameAdapter(this));
+        
+        String attrName = "newAttribute" + uniqAttrId;
+        ++uniqAttrId;
+        String attrType = "Object";
+        AccessModifier attrAccessMod = AccessModifier.Private;
+        TextFigure attrFigure = new TextFigure(attrAccessMod.getSymbol() + " " + attrName + " : " + attrType);
+        getModel().addAttribute(new UmlAttributeModel(attrAccessMod, attrName, attrType));        
+        attributesCompartment.add(attrFigure);
+        attrFigure.addFigureListener(new AttributeAdapter(this));
+        
+        AccessModifier methodAccessMod = AccessModifier.Public;
+        String methodName = "newMethod" + uniqMethodId;
+        List<UmlAttributeModel> methodParams = new ArrayList<UmlAttributeModel>();
+        //getModel().addMethod(new UmlMethodModel(methodAccessMod, methodName, methodParams));
         methodsCompartment.add(new TextFigure("+ newMethod()"));
-
-        ResourceBundleUtil labels = ResourceBundleUtil.getBundle("ui.UmlClass.Labels");
+        
+        //ResourceBundleUtil labels = ResourceBundleUtil.getBundle("ui.UmlClass.Labels");
+    }
+    
+    private TextFigure getNameFigure() {
+    	ListFigure nameCompartment = (ListFigure) this.getChild(0);
+    	TextFigure nameFigure = (TextFigure) nameCompartment.getChild(0);
+    	return nameFigure;
+    }
+    
+    private ListFigure getAttributesCompartment() {
+    	ListFigure attributesCompartment = (ListFigure) this.getChild(2);
+    	return attributesCompartment;
+    }
+    
+    private ListFigure getMethodsCompartment() {
+    	ListFigure methodsCompartment = (ListFigure) this.getChild(4);
+    	return methodsCompartment;
+    }
+    
+    @Override
+    public UmlClassFigure clone() {
+    	UmlClassFigure that = (UmlClassFigure) super.clone();
+    	that.setModel(new UmlClassModel());
+        that.myAssociationFigures = new HashSet<LineConnectionFigure>();
+        //that.getNameFigure().setText("newClass");
+        List<Figure> attrFigureList = that.getAttributesCompartment().getChildren();
+        for(int i = 0; i < attrFigureList.size(); ++i) {
+        	((TextFigure)attrFigureList.get(i)).addFigureListener(new AttributeAdapter(this));
+        }
+        that.getNameFigure().addFigureListener(new ClassNameAdapter(that));
+        return that;
     }
     
     @Override
@@ -141,19 +279,25 @@ public class UmlClassFigure extends GraphicalCompositeFigure {
     		public final static String id = "edit.addAttribute";
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// add a new attribute text figure
 				ResourceBundleUtil labels = ResourceBundleUtil.getBundle("ui.UmlClass.Labels");
-		        labels.configureAction(this, id);
-		        ((ListFigure)getChild(2)).add(new TextFigure("- newAttribute: Object"));				
+				labels.configureAction(this, id);
+		        // add a new attribute text figure
+				getAttributesCompartment().add(new TextFigure("- newAttribute" + uniqAttrId + ": Object"));
+				//TODO: map to model
+		        ++uniqAttrId;
 			}
     	});
     	actions.add(new AbstractSelectionAction(null) {
     		public final static String id = "edit.addMethod";
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				ResourceBundleUtil labels = ResourceBundleUtil.getBundle("ui.UmlClass.Labels");
+		        labels.configureAction(this, id);
 				// add a new method text figure
-				 ((ListFigure)getChild(4)).add(new TextFigure("+ newMethod()"));
-			}			
+		        getMethodsCompartment().add(new TextFigure("+ newMethod" + uniqMethodId + "()"));
+		        //TODO: map to model
+	        	++uniqMethodId;
+			}
     	});
     	
     	return actions;        
