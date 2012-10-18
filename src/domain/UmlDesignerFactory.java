d54831e43fc2b9fccd1ee940abb9bfbfba34ac82
hendleyr <hendleyr@gmail.com>
2012-10-18 12:12:16 -0500

    Update src/domain/UmlDesignerFactory.java
    Implemented the DOMFactory for uml class diagrams.

--- src/domain/UmlDesignerFactory.java
+++ src/domain/UmlDesignerFactory.java
@@ -1,8 +1,39 @@
 package domain;
 
-
+import org.jhotdraw.draw.DefaultDrawing;
+import org.jhotdraw.draw.GraphicalCompositeFigure;
+import org.jhotdraw.draw.GroupFigure;
+import org.jhotdraw.draw.ListFigure;
+import org.jhotdraw.draw.TextAreaFigure;
+import org.jhotdraw.draw.TextFigure;
 import org.jhotdraw.xml.DefaultDOMFactory;
+import ui.UmlClass.SeparatorFigure;
+import ui.UmlClass.UmlClassFigure;
+import domain.UmlClass.AccessModifier;
 
 public class UmlDesignerFactory extends DefaultDOMFactory {
+	private final static Object[][] classTagArray = {
+        { DefaultDrawing.class, "UmlDiagram" },
+        { UmlClassFigure.class, "class" },
+        { GraphicalCompositeFigure.class, "compartment" },
+        { ListFigure.class, "list" },
+        { TextFigure.class, "text" },
+        { GroupFigure.class, "g" },
+        { TextAreaFigure.class, "ta" },
+        { SeparatorFigure.class, "separator" }
+	};
 	
+	private final static Object[][] enumTagArray = {
+		{ AccessModifier.class, "accessMod" }		
+	};
+	
+    /** Creates a new instance. */
+    public UmlDesignerFactory() {
+        for (Object[] o : enumTagArray) {
+        	addEnumClass((String) o[1], (Class) o[0]);
+        }
+        for (Object[] o : classTagArray) {
+            addStorableClass((String) o[1], (Class) o[0]);
+        }
+    }
 }
