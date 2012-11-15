9e9324f4581e3bb69e403d9cd5919f43e4efb484
kmartone <martone@uwalumni.com>
2012-11-15 17:50:17 -0600

    UmlClassModel toStringTests 1-7

--- test/domain/UmlClass/TestUmlClassModel.java
+++ test/domain/UmlClass/TestUmlClassModel.java
@@ -13,17 +13,31 @@ public class TestUmlClassModel{
 	private UmlClassModel testModel;
 	private UmlClassModel anotherModel;
 	private UmlClassModel unassociatedModel;
+	private UmlClassModel testClass1a;
+	private UmlClassModel testClass1b;
+	private UmlClassModel testClass2;
+	private UmlClassModel testClass3;
+	private UmlClassModel testClass4;
+	private UmlClassModel testClass5;
+	private UmlClassModel testClass6;
+	private UmlClassModel testClass7;
 	private UmlAttributeModel testAttribute;
 	private UmlAttributeModel anotherAttribute;
 	private UmlAttributeModel nonexistentAttribute;
+	private UmlAttributeModel testAttribute5;
 	private UmlMethodModel testMethod;
 	private UmlMethodModel anotherMethod;
 	private UmlMethodModel nonexistentMethod;
+	private UmlMethodModel testMethod4;
+	private UmlMethodModel testMethod5;
+	private UmlMethodModel testMethod6;
+	private UmlMethodModel testMethod7;
+	private ArrayList<UmlAttributeModel> testAttributeList5;
 	
 	@Before
 	public void setUp() throws Exception {
 		targetModel = new UmlClassModel(AccessModifier.Public, "targetModel");
-		testModel = new UmlClassModel();
+		testModel = new UmlClassModel();	
 		anotherModel = new UmlClassModel(AccessModifier.Public, "anotherModel");
 		unassociatedModel = new UmlClassModel();
 		testAttribute = new UmlAttributeModel();
@@ -32,6 +46,33 @@ public class TestUmlClassModel{
 		testMethod = new UmlMethodModel();
 		anotherMethod = new UmlMethodModel(AccessModifier.Private, "Object", "anotherMethod", new ArrayList<UmlAttributeModel>());
 		nonexistentMethod = new UmlMethodModel(AccessModifier.Private, "Object", "nonexistentMethod", new ArrayList<UmlAttributeModel>());
+		
+		/*the following objects are used for testing the toString method
+		 * Naming convention: "test" + ObjectType + TestNumber
+		 * The ObjectType is Class, Method, or Attribute.
+		 * The TestNumber indicate for which test (see the comments in testToString) the Object is used.
+		 */
+		//testModels
+		testClass1a = new UmlClassModel();
+		testClass1b = new UmlClassModel(AccessModifier.Private, "CustomClassTest");
+		testClass2 = new UmlClassModel(AccessModifier.Public, "AbstractClassTest");
+		testClass3 = new UmlClassModel(AccessModifier.Public, "InterfaceTest");
+		testClass4 = new UmlClassModel(AccessModifier.Public, "PlainMethodTest");
+		testClass5 = new UmlClassModel(AccessModifier.Public, "MethodWithParamTest");
+		testClass6 = new UmlClassModel(AccessModifier.Public, "StaticMethodTest");
+		testClass7 = new UmlClassModel(AccessModifier.Public, "AbstractMethodTest");
+		
+		//testMethods
+		testMethod4 = new UmlMethodModel(AccessModifier.Private, "void", "plainMethod", new ArrayList<UmlAttributeModel>());
+		testMethod5 = new UmlMethodModel(AccessModifier.Private, "void", "methodWithParam", new ArrayList<UmlAttributeModel>());
+		testMethod6 = new UmlMethodModel(AccessModifier.Private, "void", "staticMethod", new ArrayList<UmlAttributeModel>());
+		testMethod7 = new UmlMethodModel(AccessModifier.Private, "void", "abstractMethod", new ArrayList<UmlAttributeModel>());
+		
+		//testAttributes
+		testAttribute5 = new UmlAttributeModel(AccessModifier.Private, "testParameter", "void");
+		
+		//testAttributeLists
+		testAttributeList5 = new ArrayList<UmlAttributeModel>();
 	}
 
 	@Test
@@ -146,5 +187,59 @@ public class TestUmlClassModel{
 		String codeSkeleton = testModel.toString();
 		assertTrue(!codeSkeleton.contains("["));
 		assertTrue(!codeSkeleton.contains("]"));
+		
+		//Test 1a: blank public class (with default name)
+		String codeSkel1a = testClass1a.toString();
+		assertEquals("public class newClass  {\n\n}\n", codeSkel1a);
+		
+		//Test 1b: blank private class (with custom name)
+		String codeSkel1b = testClass1b.toString();
+		assertEquals("private class CustomClassTest  {\n\n}\n", codeSkel1b);
+		
+		//Test 2: default abstract class
+		testClass2.setAbstractFlag(true);
+		String codeSkel2 = testClass2.toString();
+		assertEquals("public abstract class AbstractClassTest  {\n\n}\n", codeSkel2);
+		
+		//Test 3: default interface
+		testClass3.setInterfaceFlag(true);
+		String codeSkel3 = testClass3.toString();
+		assertEquals("public interface InterfaceTest  {\n\n}\n", codeSkel3);
+		
+		//Test 4: class with a non-static, non-abstract method
+		testClass4.addMethod(testMethod4);
+		String codeSkel4 = testClass4.toString();
+		assertEquals("public class PlainMethodTest  {\n\n\tprivate void plainMethod () {\n\n\t}\n}\n", codeSkel4);
+		
+		//Test 5: class with a method with a parameter
+		testAttributeList5.add(testAttribute5);
+		testMethod5.setParameters(testAttributeList5);
+		testClass5.addMethod(testMethod5);
+		String codeSkel5 = testClass5.toString();
+		assertEquals("public class MethodWithParamTest  {\n\n\tprivate void methodWithParam (void testParameter) {\n\n\t}\n}\n", codeSkel5);
+		
+		//Test 6: class with a static method
+		testMethod6.setStaticFlag(true);
+		testClass6.addMethod(testMethod6);
+		String codeSkel6 = testClass6.toString();
+		assertEquals("public class StaticMethodTest  {\n\n\tprivate static void staticMethod () {\n\n\t}\n}\n", codeSkel6);
+		
+		//Test 7: class with an abstract method
+		testMethod7.setAbstractFlag(true);
+		testClass7.addMethod(testMethod7);
+		String codeSkel7 = testClass7.toString();
+		assertEquals("public class AbstractMethodTest  {\n\n\tprivate abstract void abstractMethod () {\n\n\t}\n}\n", codeSkel7);
+		
+		//TODO: Test 8: class with a non-static attribute
+		
+		//TODO: Test 9: class with a static attribute
+		
+		//TODO: Test 10: class with an aggregation association
+		
+		//TODO: Test 11: class with a dependency association
+		
+		//TODO: Test 12: class that inherits from another class
+		
+		//TODO: Test 13: a class with both a method and an attribute
 	}
 }
